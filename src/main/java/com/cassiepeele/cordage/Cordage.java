9ecf7dd6e5379b1856cb63815cd2b27ff0295297
Cassie Peele <cassietpeele@gmail.com>
2023-07-05 16:41:23 -0400

    Updated to Minecraft 1.20

--- src/main/java/com/cassiepeele/cordage/Cordage.java
+++ src/main/java/com/cassiepeele/cordage/Cordage.java
@@ -5,7 +5,7 @@ import com.mojang.logging.LogUtils;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -44,9 +44,9 @@ public class Cordage
 
     }
 
-    private void addCreative(CreativeModeTabEvent.BuildContents event)
+    private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-        if(event.getTab() == CreativeModeTabs.INGREDIENTS) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
             event.accept(ModItems.SINEW);
         }
     }
